941c317001ad40e60bd9e0db48e9723aeaf830df
Gergo Buglyo <buglyogergo@gmail.com>
2023-08-24 22:03:40 +0200

    Created meta section style

--- frontend/src/components/meta_data/Meta.jsx
+++ frontend/src/components/meta_data/Meta.jsx
@@ -1,35 +1,44 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Meta() {
-  const dispatch = useDispatch();
   const { product } = useSelector((state) => state.products);
 
   const ProductDetails = () => {
     return (
       <>
         <div className="row-start-1 col-start-1 row-span-3  flex flex-col items-center justify-center">
-          <p className="text-xl">{product.title}</p>
+          <p className="text-2xl font-bold">{product.title}</p>
         </div>
-        <div className="row-start-1 col-start-2 row-span-3 flex">
-          <div className="flex flex-col text-center justify-center w-1/2">
-            <h2 className="text-lg underline">Category:</h2>
-            <p className="text-lg">{product.category}</p>
+        <div className="row-start-1 col-start-2 row-span-3 flex border-r-2 border-l-2">
+          <div className="flex flex-col w-1/2">
+            <div className="flex items-end justify-center h-1/2">
+              <h2 className="text-xl font-bold">Category</h2>
+            </div>
+            <div className="flex justify-center h-1/2">
+              <p className="text-xl">{product.category}</p>
+            </div>
           </div>
           <div className="flex flex-col text-center justify-center w-1/2">
-            <h2 className="text-lg underline">Brand:</h2>
-            <p className="text-lg">{product.brand}</p>
+            <div className="flex items-end justify-center h-1/2">
+              <h2 className="text-xl font-bold">Brand</h2>
+            </div>
+            <div className="flex justify-center h-1/2">
+              <p className="text-xl">{product.brand}</p>
+            </div>
           </div>
         </div>
         <div className="row-start-2 col-start-3 flex justify-center">
-          <img src={product.thumbnail} alt={product.title} />
+          {product.thumbnail && (
+            <img src={product.thumbnail} alt={product.title} />
+          )}
         </div>
       </>
     );
   };
 
   return (
-    <div className="row-start-2 col-start-2 row-span-3 col-span-full grid grid-cols-3 grid-rows-3 gap-0 p-1 border-x-2">
+    <div className="row-start-2 col-start-2 row-span-3 col-span-full grid grid-cols-3 grid-rows-3 gap-0 border-x-2">
       {product.id && <ProductDetails />}
     </div>
   );
